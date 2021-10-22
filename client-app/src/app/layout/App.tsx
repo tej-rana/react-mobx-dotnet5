afcc6e29834360a136321a91d5fb0cb34e227a38
Tej Rana <tej@allstrata.tech>
2021-10-22 10:10:19 +1000

    Clean up after merge

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,6 +1,4 @@
 import React, {useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
 import axios from 'axios';
 import {useState} from 'react';
 import {Header, List} from 'semantic-ui-react'
