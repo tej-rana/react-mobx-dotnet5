e0699f55f214e67906865b3d1d82675ab74a57ca
Tej Rana <tej@allstrata.tech>
2021-10-25 10:42:54 +1000

    Added MOBX

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -7,7 +7,7 @@ const sleep = (delay: number) => {
     });
 }
 
-axios.defaults.baseURL = 'http://localhost:5000/api';
+axios.defaults.baseURL = 'http://localhost:5100/api';
 
 axios.interceptors.response
     .use(async response => {
